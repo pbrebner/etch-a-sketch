8ef5fe8109e557957b3ba5986d620e0374692b57
Patrick Brebner <patrick.brebner@gmail.com>
2023-01-16 17:29:42 -0800

    Add Button to change size of grid

--- script.js
+++ script.js
@@ -2,20 +2,57 @@
 
 const gridContainer = document.querySelector('.gridContainer')
 
-// Create 16x16 grid of divs and place in the gridContainer
+// Create 16x16 grid (default size) of divs and place in the gridContainer
+const defaultGridSize = 16;
 
-for (i = 1; i <= 256; i++) {
+for (i = 1; i <= defaultGridSize * defaultGridSize; i++) {
     const gridBox = document.createElement('div');
     gridBox.setAttribute('class', 'gridBox');
-    gridBox.innerHTML = "box";
     gridContainer.appendChild(gridBox);
 }
 
-
+//Hover Event
 const gridBoxes = document.querySelectorAll('.gridBox');
 
 gridBoxes.forEach((item) => {
     item.addEventListener('mouseover', function (e) {
         e.target.style.background = 'grey';
     });
+})
+
+//Button to adjust grid size
+const gridSizeBtn = document.querySelector('.gridSizeBtn')
+
+gridSizeBtn.addEventListener('click', () => {
+    let gridSize = Number(window.prompt('What size of grid do you want? (Min 2, Max 100)', 16));
+
+    while(true) { 
+        if (gridSize == 0){
+            gridSize = defaultGridSize;
+            break
+        }
+        else if (gridSize > 100 || gridSize < 2) {
+            gridSize = Number(window.prompt('Chosen size was either too big or too small. What size of grid do you want? (Min 2, Max 100)', 16));
+            continue
+        }
+        else break
+    }
+
+    gridContainer.innerHTML = '';
+
+    for (i = 1; i <= (gridSize * gridSize); i++) {
+        const gridBox = document.createElement('div');
+        gridBox.setAttribute('class', 'gridBox');
+        gridBox.style.width = `${960/gridSize -2}px`;
+        gridBox.style.height = `${960/gridSize -2}px`;
+        gridContainer.appendChild(gridBox);
+    }
+
+    const gridBoxes = document.querySelectorAll('.gridBox');
+
+    gridBoxes.forEach((item) => {
+        item.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'grey';
+        });
+    })
 })
